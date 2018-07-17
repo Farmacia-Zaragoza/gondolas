99c945575e647a734d0d8bc02a0363771e42bcae
Onilkabbo <33430369+Onilkabbo@users.noreply.github.com>
2018-07-17 13:15:24 +0600

    Update common.js

--- cassets/js/common.js
+++ cassets/js/common.js
@@ -1,3 +1,8 @@
+/*
+  common.js - V_0_0_2
+  Date: 10-07-2018
+*/
+
 $(window).ready(function(){
 
         //Relode On Resize / ZoomIn/Out
@@ -10,7 +15,7 @@ $(window).ready(function(){
         //         return;
         //     }
         // });
-
+    /* script for language section starts */
     function scrollLeftToRight(){
         var div = $('.scrolling_wrapper');
         var remWidth = div[0].scrollWidth - $(this).width();
@@ -47,15 +52,16 @@ $(window).ready(function(){
     $('.flags_backward').mouseleave(function(){
         $('.scrolling_wrapper').stop();
     }); 
+    /* script for language section ends here */
 
-
+    /* script for flex_caption section starts */
     function slideUpToDown(){
-        var div = $('.caption_overflow');
+        var div = $('.marquee_wrapper');
         var remHeight = div[0].scrollHeight - $(this).height();
         var scrollableHeight = remHeight - div.scrollTop();
         var pos = div.scrollTop();
         var remainingTime = (scrollableHeight - pos) * 100 / 10; //here 100 is a speed
-        $('.caption_overflow').animate({
+        $('.marquee_wrapper').animate({
             scrollTop:remHeight
         },{
             duration: remainingTime,
@@ -64,27 +70,92 @@ $(window).ready(function(){
     }
 
     function slideDownToToUp(){
-        var div = $('.caption_overflow');
+        var div = $('.marquee_wrapper');
         var remHeight = div[0].scrollHeight - $(this).height();
         var scrollableHeight = remHeight - div.scrollTop();
         var pos = div.scrollTop();
         var remainingTime = (pos * 100) / 10; //here 100 is a speed
-        $('.caption_overflow').animate({
+        $('.marquee_wrapper').animate({
             scrollTop: 0
         },{
             duration: remainingTime,
             easing: "linear",
         });        
     }
-    $('.downward_dir').mouseenter(slideDownToToUp);
+    $('.downward_dir').mouseenter(slideUpToDown);
     $('.downward_dir').mouseleave(function(){
-        $('.caption_overflow').stop();
+        $('.marquee_wrapper').stop();
     });
-    $('.upward_dir').mouseenter(slideUpToDown);  
+    $('.upward_dir').mouseenter(slideDownToToUp);  
 
     $('.upward_dir').mouseleave(function(){
-        $('.caption_overflow').stop();
+        $('.marquee_wrapper').stop();
     });
+    /* script for flex_caption section ends here */
+
+        /* script for contact section starts */
+    function contactLeftToRight(){
+        var div = $('.contact_desc');
+        var remWidth = div[0].scrollWidth - $(this).width();
+        var scrollableHeight = remWidth - div.scrollLeft();
+        var pos = div.scrollLeft();
+        var remainingTime = (scrollableHeight - pos) * 100 / 10; //here 100 is a speed
+        $('.contact_desc').animate({
+            scrollLeft:remWidth
+        },{
+            duration: remainingTime,
+            easing: "linear",
+        }); 
+    }
+
+    function contactRightToLeft(){
+        var div = $('.contact_desc');
+        var remWidth = div[0].scrollWidth - $(this).width();
+        var scrollableHeight = remWidth - div.scrollLeft();
+        var pos = div.scrollLeft();
+        var remainingTime = (pos * 100) / 10;
+        $('.contact_desc').animate({
+            scrollLeft: 0
+        },{
+            duration: remainingTime,
+            easing: "linear",
+        });        
+    }
+    $('.contact_left_dir').mouseenter(contactRightToLeft);
+    $('.contact_left_dir').mouseleave(function(){
+        $('.contact_desc').stop();
+    });
+    $('.contact_right_dir').mouseenter(contactLeftToRight);  
+
+    $('.contact_right_dir').mouseleave(function(){
+        $('.contact_desc').stop();
+    }); 
+    /* script for contact section ends here */
+
+            //Global blazy module starts
+            var bLazy = new Blazy({
+                selector: '.b-lazy',
+                breakpoints: [{
+                    width: 767, //max-width
+                    src: 'data-src-small'
+                }],
+
+                success: function(ele) {
+                    $(ele).closest('.image-wrapper').addClass('image-loaded');
+                    //$(ele).closest('li.img').addClass('b-loaded');
+                    console.log(ele);
+                },
+                error: function(ele, msg) {
+
+                    var image = $(ele)[0];
+                    if (msg === 'missing') {
+                        console.warn("Custom ERROR: ", image, " data-src is missing\n");
+                    } else if (msg === 'invalid') {
+                        console.warn("Custom ERROR: ", image, " data-src is invalid\n");
+                    }
+                }
+            });
+            //Global blazy module finishes
 
 });
 
@@ -93,7 +164,7 @@ $(window).load(function() {
   // The slider being synced must be initialized first
     var item_width;
     var window_width = window.innerWidth;
-    console.log(window_width);
+    //console.log(window_width);
     if(window_width < 768) {
         item_width = 150;
     }
